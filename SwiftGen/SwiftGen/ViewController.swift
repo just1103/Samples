0852185000ce69e56ad2ffd9a1da0ca8d3601628
just1103 <hyojuson1103@gmail.com>
2024-07-02 14:58:23 +0900

    feat: swiftGen generated Color/Image applied

--- SwiftGen/SwiftGen/ViewController.swift
+++ SwiftGen/SwiftGen/ViewController.swift
@@ -4,7 +4,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Color
+        view.backgroundColor = UIColor.customPurple
+        
+        // Image
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage.iconSettingP5.withTintColor(.white), for: .normal)
+        button.setImage(UIImage.iconDoneP5.withTintColor(.white), for: .selected)
+        button.addAction(UIAction { action in
+            guard let btn = action.sender as? UIButton else { return }
+            btn.isSelected = !btn.isSelected
+        }, for: .touchUpInside)
+        
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 50),
+            button.heightAnchor.constraint(equalToConstant: 50),
+        ])
     }
 
 
