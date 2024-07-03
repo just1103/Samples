32df72b57e05168e6f13d3803b43ec921187282f
just1103 <hyojuson1103@gmail.com>
2024-07-03 15:56:01 +0900

    MultiModule / feat: workspace, 3 module (project file) added

---
+++ MultiModule/ModuleA/ModuleA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModuleA
+//
+//  Created by 손효주 on 7/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
