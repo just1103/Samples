867175e11c7c33493667518a4d8f0702ba53ae2c
just1103 <hyojuson1103@gmail.com>
2024-08-04 23:23:16 +0900

    feat: ScrollView BottomInset / ScrollViewReader 활용하여 해결

--- ScrollViewBottomInset/ScrollViewBottomInset/ContentView.swift
+++ ScrollViewBottomInset/ScrollViewBottomInset/ContentView.swift
@@ -10,55 +10,53 @@ struct ContentView: View {
     @State var bottomInset: CGFloat = 0
     
     var body: some View {
-//        GeometryReader { geometry in
-        ScrollView {
-            VStack(spacing: 0) {
-                Rectangle()
-                    .foregroundStyle(Color.yellow)
-                    .frame(height: 300)
-                
-                TextField("입력해봐요", text: $text, axis: .vertical)
-                    .multilineTextAlignment(.leading)
-                    .padding(.leading, 50)
-                    .focused($isFocused)
-                    .background(Color.green)
-//                    .contentMargins(.bottom, bottomInset, for: .scrollContent) // x
-//                    .padding(.bottom, bottomInset) // x
-//                    .safeAreaPadding(.bottom, bottomInset) // x
-//                    .safeAreaInset(edge: .bottom, content: { // x
-//                        Rectangle()
-//                            .foregroundStyle(Color.purple)
-//                            .frame(height: bottomInset)
-//                    })
 
-//                Spacer()
+        ScrollViewReader { proxy in
+            
+            ScrollView {
+                VStack(spacing: 0) {
+                    Rectangle()
+                        .foregroundStyle(Color.yellow)
+                        .frame(height: 300)
+                    
+                    TextField("입력해봐요", text: $text, axis: .vertical)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 50)
+                        .focused($isFocused)
+                        .background(Color.green)
+                        .onChange(of: text) { _ in
+                            // !!!: When text changes, scroll to the end if needed
+                            if isFocused {
+                                DispatchQueue.main.async {
+                                    proxy.scrollTo("bottom", anchor: .bottom)
+                                }
+                            }
+                        }
+
+                    Spacer()
+                        .id("bottom")
+                }
+                .background(Color.gray)
+                .background(Color.purple)
             }
-            .background(Color.gray)
-//            .contentMargins(.bottom, bottomInset, for: .scrollContent) // x
-//            .padding(.bottom, bottomInset) // x
-//            .safeAreaPadding(.bottom, bottomInset) // x
-//            .safeAreaInset(edge: .bottom, content: { // x
-//                Rectangle()
-//                    .foregroundStyle(Color.purple)
-//                    .frame(height: bottomInset)
-//            })
-            .background(Color.purple)
+            .background(Color.blue)
+            .contentMargins(.horizontal, 24, for: .scrollContent)
+    //        .contentMargins(.bottom, bottomInset, for: .scrollContent) // x
+    //        .padding(.bottom, bottomInset) // x
+    //        .safeAreaPadding(.bottom, bottomInset) // x
+    //        .safeAreaInset(edge: .bottom, content: { // x
+    //            Rectangle()
+    //                .foregroundStyle(Color.purple)
+    //                .frame(height: bottomInset)
+    //        })
+            .background(Color.mint)
+            .onReceive(keyboardManager.$keyboardHeight) { height in
+                print("@@@ inset 넣을게 ...", height)
+                self.bottomInset = height
+            }
+            
         }
-        .background(Color.blue)
-        .contentMargins(.horizontal, 24, for: .scrollContent)
-//        .contentMargins(.bottom, bottomInset, for: .scrollContent) // x
-//        .padding(.bottom, bottomInset) // x
-//        .safeAreaPadding(.bottom, bottomInset) // x
-//        .safeAreaInset(edge: .bottom, content: { // x
-//            Rectangle()
-//                .foregroundStyle(Color.purple)
-//                .frame(height: bottomInset)
-//        })
-        .background(Color.mint)
-        .onReceive(keyboardManager.$keyboardHeight) { height in
-            print("@@@ inset 넣을게 ...", height)
-            self.bottomInset = height
-        }
+        
     }
 }
 
