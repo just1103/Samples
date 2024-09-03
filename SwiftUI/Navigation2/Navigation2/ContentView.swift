e697567756dcab31b47d5ee9299f84b0860341b7
just1103 <hyojuson1103@gmail.com>
2024-09-03 19:12:35 +0900

    feat: Navigation2 prj / TabBar Navigation added

---
+++ SwiftUI/Navigation2/Navigation2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation2
+//
+//  Created by 손효주 on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Navigation2/Navigation2/Navigation2App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation2App.swift
+//  Navigation2
+//
+//  Created by 손효주 on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
