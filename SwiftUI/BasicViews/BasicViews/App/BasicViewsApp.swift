afe4b117a0e7bc9292a5cd98c2aa211426bc5a05
just1103 <hyojuson1103@gmail.com>
2025-01-28 21:26:30 +0900

    feat: BasicViews / BindingView added

--- SwiftUI/BasicViews/BasicViews/App/BasicViewsApp.swift
+++ SwiftUI/BasicViews/BasicViews/App/BasicViewsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BasicViewsApp: App {
     var body: some Scene {
         WindowGroup {
-            RedrawingTextView()
+            BindingView()
         }
     }
 }
---
+++ SwiftUI/BasicViews/BasicViews/Wrappers/BindingView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct BindingView: View {
+    
+    @State private var isPlaying: Bool = false
+    
+    var body: some View {
+        Text(isPlaying ? "Paused ..." : "Playing ...")
+        
+        ChildView(isPlaying: $isPlaying)
+    }
+}
+
+struct ChildView: View {
+    
+    // 상위 뷰가 소유한 프로퍼티이므로
+    // 하위 뷰는 소유권/저장공간이 없음
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Button(isPlaying ? "Pause" : "Play") {
+            // 하위 뷰 <-> 상위 뷰 변경사항 공유됨
+            isPlaying.toggle()
+        }
+        .foregroundStyle(Color.white)
+        .padding()
+        .background(Color.random)
+    }
+}
+
+#Preview {
+    BindingView()
+}
