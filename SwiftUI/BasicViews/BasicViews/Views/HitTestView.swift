2f151f04bc6621770132d24b3b236a7dca16340e
just1103 <hyojuson1103@gmail.com>
2025-02-02 18:11:18 +0900

    feat: HitTestView / FavoriteButton on List added

--- SwiftUI/BasicViews/BasicViews/Views/HitTestView.swift
+++ SwiftUI/BasicViews/BasicViews/Views/HitTestView.swift
@@ -1,89 +1,124 @@
 import SwiftUI
 
+enum NavigationDestination: Hashable {
+}
+
 struct HitTestView: View {
     
     @State var isFavorite = false
+    @State var path: [NavigationDestination] = []
     
     var body: some View {
-        // overlay로 버튼 올리면 탭 이벤트가 뺏기나?
-        // 둘다 잘됨
-        Rectangle()
-            .foregroundStyle(Color.purple)
-            .frame(width: 200, height: 200)
-            .onTapGesture {
-                print("@@@ Rectangle 1 tapped !")
-            }
-            .overlay(alignment: .topTrailing) {
+        
+        NavigationStack(path: $path) {
+            
+            // overlay로 버튼 올리면 탭 이벤트가 뺏기나?
+            // 둘다 잘됨
+            Rectangle()
+                .foregroundStyle(Color.purple)
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    print("@@@ Rectangle 1 tapped !")
+                }
+                .overlay(alignment: .topTrailing) {
+                    Button {
+                        isFavorite.toggle()
+                    } label: {
+                        Image(systemName: isFavorite ? "star.fill" : "star")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                    }
+                    .offset(x: -10, y: 10)
+                }
+            
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(Color.purple)
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        print("@@@ Rectangle 2 tapped !")
+                    }
+                
                 Button {
                     isFavorite.toggle()
                 } label: {
                     Image(systemName: isFavorite ? "star.fill" : "star")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 80, height: 80)
+                        .frame(width: 50, height: 50)
                 }
-                .offset(x: -20, y: 20)
             }
-        
-        ZStack {
-            Rectangle()
-                .foregroundStyle(Color.purple)
-                .frame(width: 200, height: 200)
-                .onTapGesture {
-                    print("@@@ Rectangle 2 tapped !")
-                }
             
-            Button {
-                isFavorite.toggle()
-            } label: {
-                Image(systemName: isFavorite ? "star.fill" : "star")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 80, height: 80)
-            }
-        }
-        
-        // MARK: - FavoriteButton
-        FavoriteButton(
-            handler: {
-                isFavorite.toggle()
-            }, isFavorite: $isFavorite
-        )
-        
-        // FIXME: 또 다른 문제
-        // image 바깥의 touchArea 탭할 때
-        // FavoriteButton 대신 Rectangle이 이벤트를 받음;
-        // 근데 이건 일반 Button도 동일하게 발생하는 문제임;;
-        Rectangle()
-            .foregroundStyle(Color.purple)
-            .frame(width: 100, height: 100)
-            .onTapGesture {
-                print("@@@ Rectangle 2-1 tapped !")
-            }
-            .overlay(alignment: .topTrailing) {
-                FavoriteButton(
-                    handler: {
-                        isFavorite.toggle()
-                    }, isFavorite: $isFavorite
-                )
-                .offset(x: -20, y: 20)
-            }
-        
-        ZStack {
-            Rectangle()
-                .foregroundStyle(Color.purple)
-                .frame(width: 100, height: 100)
-                .onTapGesture {
-                    print("@@@ Rectangle 2-2 tapped !")
-                }
-            
+            // MARK: - FavoriteButton on Button
             FavoriteButton(
                 handler: {
                     isFavorite.toggle()
                 }, isFavorite: $isFavorite
             )
+            
+            // FIXME: 또 다른 문제
+            // image 바깥의 touchArea 탭할 때
+            // FavoriteButton 대신 Rectangle이 이벤트를 받음;
+            // 근데 이건 일반 Button도 동일하게 발생하는 문제임;;
+            Rectangle()
+                .foregroundStyle(Color.purple)
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    print("@@@ Rectangle 2-1 tapped !")
+                }
+                .overlay(alignment: .topTrailing) {
+                    FavoriteButton(
+                        handler: {
+                            isFavorite.toggle()
+                        }, isFavorite: $isFavorite
+                    )
+                    .offset(x: -20, y: 20)
+                }
+            
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(Color.purple)
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        print("@@@ Rectangle 2-2 tapped !")
+                    }
+                
+                FavoriteButton(
+                    handler: {
+                        isFavorite.toggle()
+                    }, isFavorite: $isFavorite
+                )
+            }
+            
+            // MARK: - FavoriteButton on List
+            ScrollView {
+                LazyHGrid(
+                    rows:  [
+                        GridItem(.fixed(100)),
+                    ],
+                    alignment: .center,
+                    spacing: 8
+                ) {
+                    ForEach(0..<3, id: \.self) { item in
+                        NavigationLink(destination: Text("Detail")) {
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(width: 100, height: 100)
+                                .overlay(alignment: .topTrailing) {
+                                    FavoriteButton(
+                                        handler: {
+                                            isFavorite.toggle()
+                                        }, isFavorite: $isFavorite
+                                    )
+                                    .offset(x: -10, y: 10)
+                                }
+                        }
+                    }
+                }
+            }
+            .padding()
         }
-        
     }
 }
 
