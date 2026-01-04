e8d0b5044ad90d3da9690a5f854d2b3c0297460d
just1103 <hyojuson1103@gmail.com>
2026-01-04 16:55:43 +0900

    minor: ThemeOption / list bgColor 수정

--- SwiftUI/ThemeOption/ThemeOption/ContentView.swift
+++ SwiftUI/ThemeOption/ThemeOption/ContentView.swift
@@ -32,8 +32,8 @@ struct ContentView: View {
                 }
                 .pickerStyle(.menu)
             }
-            .listRowBackground(colors.backgroundPrimary)
         }
+        .listRowBackground(colors.backgroundPrimary)
         .preferredColorScheme(viewModel.preferredColorScheme) // system dynamic color를 사용중이라면 필요함
     }
 }
